99e353bd24a984c2d0cde2321fbe01cc3f5bb795
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2020-05-01 09:10:19 -0300

    Fix scroll on <700px

--- src/app/components/job-list/job-list.component.ts
+++ src/app/components/job-list/job-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  ContentChild,
+} from '@angular/core';
 import { PortfolioService } from '../../services/portfolio.service';
 
 @Component({
@@ -6,13 +13,35 @@ import { PortfolioService } from '../../services/portfolio.service';
   templateUrl: './job-list.component.html',
   styleUrls: ['./job-list.component.sass'],
 })
-export class JobListComponent implements OnInit {
+export class JobListComponent implements OnInit, AfterViewInit {
   constructor(private portfolioService: PortfolioService) {}
-  private jobList: Array<object> = [];
+  @ViewChild('jobListElement', { static: false }) jobListElement: ElementRef;
+
+  public jobList: Array<object> = [];
 
   ngOnInit(): void {
     this.portfolioService.getPortfolioData().subscribe((data) => {
       this.jobList.push(...data);
     });
+  }
+
+  ngAfterViewInit(): void {
+    window.addEventListener('wheel', this.captureScroll, false);
+    console.log(this.jobListElement);
+  }
+
+  private captureScroll(event) {
+    if (window.innerWidth >= 700) {
+      event.preventDefault();
+
+      if (event.deltaY < 0) {
+        console.log('scrolling up ' + event.deltaY + ' pixels');
+      } else if (event.deltaY > 0) {
+        console.log('scrolling down ' + event.deltaY + ' pixels');
+        console.log(this.jobListElement);
+        // this.jobListElement.nativeElement.scrollRight = event.deltaY;
+        // console.log(this.jobListElement);
+      }
+    }
   }
 }
--- src/app/components/single-job/single-job.component.ts
+++ src/app/components/single-job/single-job.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-single-job',
   templateUrl: './single-job.component.html',
-  styleUrls: ['./single-job.component.sass']
+  styleUrls: ['./single-job.component.sass'],
 })
 export class SingleJobComponent implements OnInit {
+  @Input() job: any;
+  @Input() jobIndex: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.jobIndex = this.jobIndex + 1;
+    this.jobIndex < 10 ? (this.jobIndex = '0' + this.jobIndex.toString()) : '';
   }
-
 }
